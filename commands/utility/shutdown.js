ba034bdfd9751422d3f6a0a36f60b69d3d7d5fa7
Daniel <daniel4scratchhelp@gmail.com>
2025-06-03 10:16:58 +1000

    first commit

---
+++ commands/utility/shutdown.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const OWNER_ID = '853820912628269088'; // Replace with your Discord user ID
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shutdown')
+        .setDescription('Shuts down the bot (owner only)'),
+    async execute(interaction) {
+        if (interaction.user.id !== OWNER_ID) {
+            return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        }
+        await interaction.reply('Shutting down...');
+        await interaction.client.destroy();
+        process.exit(0);
+    },
+};
